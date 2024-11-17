ad9f9065b74f6295ab2397c08f6c5cccc73e3a01
IhorC. <igorchua@gmail.com>
2024-11-16 22:18:24 -0600

    feat: latest activities added;

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,7 +6,7 @@ import LatestActivitiesSkeleton from "./skeleton/latest-activities";
 import ProjectCardSkeleton from "./skeleton/project-card";
 import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
 import { DashboardTotalCountCard } from "./home/total-count-card";
-
+import { LatestActivities } from "./home/latest-activities";
 
 export {
     UpcomingEvents, 
@@ -17,6 +17,7 @@ export {
     LatestActivitiesSkeleton,
     ProjectCardSkeleton,
     UpcomingEventsSkeleton,
-    
+
     DashboardTotalCountCard,
+    LatestActivities
 };
